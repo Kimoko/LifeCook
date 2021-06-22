8f13865306d6aca4028d767a530b1d286faa83b8
Kirill <46571896+Kimoko@users.noreply.github.com>
2021-06-23 00:01:45 +0500

    qwe

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,6 +8,7 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from '../Pages/login';
+import * as ROUTES from '../constants/routes';
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -25,20 +26,16 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Header()  {
   const classes = useStyles();
-
     return (
       <>
       <div className={classes.root}>
       <AppBar position="static" color="255, 233, 222, 72">
         <Toolbar>
-          {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton> */}
           <Typography variant="h4" className={classes.title}>
               lifeCook
           </Typography>
-          <Button color="inherit" href="/login">Login</Button>
-          <Button color="inherit" href="/register">Register</Button>
+          <Button color="inherit" href={ROUTES.LOGIN}>Login</Button>
+          <Button color="inherit" href={ROUTES.REGISTER}>Register</Button>
         </Toolbar>
       </AppBar>
     </div>
