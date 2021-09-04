be4da3a0172be7e341df050afb4fb5182858b1f9
Kirill <46571896+Kimoko@users.noreply.github.com>
2021-09-04 09:19:48 +0500

    qqq

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { hexToRgb, makeStyles, rgbToHex } from '@material-ui/core/styles';
+import * as ROUTES from '../constants/routes';
+import { Link } from 'react-router-dom';
 
    export default function Footer()  {
       return (
         <div>
            <div>© (copyright), 2021 |
                Email: example@gmail.com |
-               num: 89044618864</div>
+              <Link href='/' >num: 89044618864</Link>
+              </div>
            
             </div>
       );
